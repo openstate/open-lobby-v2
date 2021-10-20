0cba111b95f2a1344a89d5ba6655e3ce36c49d12
Breyten Ernsting <bje@dds.nl>
2021-10-20 10:59:30 +0200

    Show results in search and add api calls for persons and organizations.

--- src/routes/Search.svelte
+++ src/routes/Search.svelte
@@ -1,9 +1,45 @@
 <h1>Search</h1>
 <p>Zoeken naar &quot;{params.query}&quot;</p>
+
+{#each $results as r}
+<div class="odd-search-result">
+<Tile format="short" on:click={(e) => handleClick(e, r)}>
+  <Header>
+    <Profile>
+    { r._source.name }
+    </Profile>
+  </Header>
+  <Body>
+  <!--
+  <Body image="https://via.placeholder.com/250x170">
+  <h3>Titel van Artikel</h3> -->
+  { r._source.description }
+  </Body>
+  <!--
+  <Actions>
+    <Action on:click={handleClick}>Deel op Twitter</Action>
+    <Action on:click={handleClick}>Deel op Facebook</Action>
+  </Actions> -->
+</Tile>
+</div>
+{/each}
+
 <script>
+import {link, push} from 'svelte-spa-router';
+import Button from '@soddk/button';
+import Profile from '@soddk/common';
+import Tile, {Header, Body, Actions, Action} from '@soddk/tile';
+
+import { results } from '../stores.js';
 import { search_request } from '../sources.js';
 // You need to define the component prop "params"
 export let params = {};
+
+function handleClick(e, r) {
+  e.preventDefault();
+		console.log('Button Clicked for ', r._source.id);
+    return false;
+	}
 </script>
 
 <script context="module">
--- src/sources.js
+++ src/sources.js
@@ -14,13 +14,38 @@ function api_request(params, index) {
       function (data) {
         console.log('got data:');
         console.dir(data);
-        results.set(data);
+        results.set(data.hits.hits);
       });
 }
 
 export function search_request(query) {
+  // http://api.opendraaideur.nl/search?query=rutte
   return api_request({
     query: query,
+    limit: 1000
+  });
+}
+
+export function find_person(per_slug) {
+  // http://api.opendraaideur.nl/persons/search?filter=id.keyword:mark-rutte
+  return api_request('persons', {
+    filter: "id.keyword:" + per_slug,
+    limit: 1000
+  });
+}
+
+export function find_organization(org_slug) {
+  // http://api.opendraaideur.nl/organizations/search?filter=id.keyword:vvd
+  return api_request('organizations', {
+    filter: "id.keyword:" + per_slug,
+    limit: 1000
+  });
+}
+
+export function find_organization_with_members(org_slug) {
+  //  http://api.opendraaideur.nl/persons/search?filter=memberships.organization.id.keyword:vvd
+  return api_request('persons', {
+    filter: "memberships.organization.id.keyword:" + org_slug,
     limit: 1000
   });
 }
