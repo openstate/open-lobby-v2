a6efab879f05eb87839a9b1f5e980ce860c07cc5
Breyten Ernsting <bje@dds.nl>
2021-10-21 16:32:50 +0200

    Link to website.

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -4,7 +4,7 @@
 <p>Wil jij ook weten waar onze politici terecht komen na hun ambtsperiode? Check het dan hier op Open Draaideur!</p>
 
 <p>Lobbyen in Nederland is niet transparant en niet evenwichtig. Dat is niet goed voor het vertrouwen in politiek en democratie. Een belangrijk onderdeel van schimmige en onevenwichtige lobby is de ‘politieke draaideur’: politici (bewindspersonen, Kamerleden) komen na hun ambtsperiode weer terug in de politiek, maar nu als belangenbehartiger van een bedrijf, branche of specifieke groep.</p>
-<p>Met deze tool schijnt Open State Foundation licht op de politieke draaideur. Zoek en vergelijk zelf waar politici die de afgelopen tien jaar zijn vertrokken, naartoe zijn gegaan. Zoek op naam, partij, bedrijf of organisatie.</p>
+<p>Met deze tool schijnt <a href="http://openstate.eu" target="_blank">Open State Foundation</a> licht op de politieke draaideur. Zoek en vergelijk zelf waar politici die de afgelopen tien jaar zijn vertrokken, naartoe zijn gegaan. Zoek op naam, partij, bedrijf of organisatie.</p>
 <div class="odd-home">
   <form class="odd-search-form">
     <input type="text" name="query" placeholder="Rutte, SP, KLM..." bind:value={query} on:change={handleSearch}>
